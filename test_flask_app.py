93d5bd81e49e6ff52e0769099e259d02539807b4
Alexander Tcherniahovski <134253868+SashaTch@users.noreply.github.com>
2023-07-22 15:17:12 +0300

    Create test_flask_app.py

---
+++ test_flask_app.py
@@ -0,0 +1,14 @@
+import pytest
+from flask import url_for
+
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.data == b'Hello, World!'
