94780c645f066f1656a49415c2feb31172a41e56
Alexander Tcherniahovski <134253868+SashaTch@users.noreply.github.com>
2023-07-30 17:27:24 +0300

    Update state.sh

--- scripts/state.sh
+++ scripts/state.sh
@@ -37,7 +37,7 @@ case "$stage" in
         test)
                 #tests
                 curl $ip:5000
-                ssh -i $ssh_key ec2-user@$ip "docker stop /$(cat /home/ec2-user/container_id.txt)"
+                ssh -i $ssh_key ec2-user@$ip "docker stop \$(cat /home/ec2-user/container_id.txt)"
                 ssh -i $ssh_key ec2-user@$ip "sudo rm -f /home/ec2-user/container_id.txt"
                 ;;
         *)
