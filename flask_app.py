6afb5baac56a27e44088f91531688869ded6fd43
Alexander Tcherniahovski <134253868+SashaTch@users.noreply.github.com>
2023-08-02 12:24:20 +0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 
-app = Flask(__name__,static_folder='static')
+app = Flask(__name__, static_folder='static', template_folder='templates')
+
 app.secret_key = 'your_secret_key'  # Replace with a secret key for session management
 
 # Login route
