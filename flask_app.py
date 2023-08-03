c96a97e2c01b886c5db4712984c99f2f09f90566
Alexander Tcherniahovski <134253868+SashaTch@users.noreply.github.com>
2023-08-03 14:56:42 +0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,53 +1,80 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
-app.secret_key = 'your_secret_key'  # Replace with a secret key for session management
+#app.secret_key = 'your_secret_key' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'  # For demonstration purposes. You should choose a secure persistent storage.
 
-# Login route
+db = SQLAlchemy(app)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 @app.route('/')
 def login():
-    if 'username' in session:
-        # If the user is already logged in, redirect to the profile page
+    if current_user.is_authenticated:
         return redirect(url_for('main_page'))
-    else:
-        # If the user is not logged in, render the login template
-        return render_template('login.html')
+    return render_template('login.html')
 
-# Login form submission route
 @app.route('/login', methods=['GET', 'POST'])
 def login_submit():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+        user = User.query.filter_by(username=username).first()
 
-        if username == 'admin' and password == 'password':
-            session['username'] = username
-            return redirect(url_for('main_page'))
-        else:
+        if user is None or not user.check_password(password):
             return render_template('login.html', error='Invalid credentials. Please try again.')
-    else:
-        if 'username' in session:
-            return redirect(url_for('main_page'))
-        else:
-            return render_template('login.html')
-        
-# Main_page route
-@app.route('/main_page')
-def profile():
-    if 'username' in session:
-        # If the user is logged in, render the profile template
-        username = session['username']
-        return render_template('profile.html', username=username)
-    else:
-        # If the user is not logged in, redirect to the login page
+
+        login_user(user)
+        return redirect(url_for('main_page'))
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if User.query.filter_by(username=username).first() is not None:
+            return render_template('register.html', error='Username already exists.')
+
+        user = User(username=username)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+
         return redirect(url_for('login'))
-# Logout route
+    return render_template('register.html')
+
+@app.route('/main_page')
+@login_required
+def main_page():
+    return render_template('profile.html', username=current_user.username)
+
 @app.route('/logout')
 def logout():
-    session.pop('username', None)
+    logout_user()
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
+    db.create_all()  # Creates database tables. Not suitable for a production environment.
     app.run(host='0.0.0.0', port=5000, debug=True)
-
