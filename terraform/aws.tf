dda9584b86a2c67f7f3d00078745729882a49a02
sasha <sasha@project.sasha.virtualbox.org>
2023-09-12 15:52:21 +0300

    configure google

---
+++ terraform/aws.tf
@@ -0,0 +1,13 @@
+terraform {
+    required_providers {
+        google = {
+            source = "hashicorp/google"
+            version = "~> 4.51.0"
+        }
+    }
+}
+
+provider "google" {
+  credentials = file("google.json")
+}
+    
