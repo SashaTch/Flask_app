591a30eaa0c8409f230ba80734f11d2dc67d1454
sasha <sasha@project.sasha.virtualbox.org>
2023-09-12 20:04:12 +0300

    gke terraform

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -9,5 +9,35 @@ terraform {
 
 provider "google" {
   credentials = file("google.json")
+  project = "pypet-k8"
+  region = "me-west1"
+  zone = "me-west1-c"
 }
-    
+
+resource "google_container_cluster" "primary" {
+    name = "gke-cluster-pypet"
+    remove_default_node_pool = true
+    initial_node_count = 1
+
+    master_auth {
+      username = ""
+      password = ""
+    }
+
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+    name = gke-node-pypet
+    cluster = google_container_cluster.primary.gke-cluster-pypet
+    node_count = 1
+
+    node_config {
+      preemptible = true
+      machine_type = "e2-medium"
+      service_account = google_service_account.pypet-admin@pypet-k8.iam.gserviceaccount.com
+      oauto_oauth_scopes = [
+        "https://www.googleapis.com/auth/logging.write",
+        "https://www.googleapis.com/auth/monitoring",
+      ]
+    } 
+}
